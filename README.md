8096868d38bf6e0acf93450f0e1f61a594dfae07
caoliang <sg_caoliang@yahoo.com.sg>
2020-02-11 21:44:11 +0800

    Upload the capstone-tf2 model files for training;

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# capstone-tf2
-Capstone project using tensor flow 2
+# Capstone-TF2 Guide
+
+## Setup Packages
+ 1. Install basic packages
+> conda env create -f environment.yml
+conda activate capstone-tf2
+
+2. Install jupyter
+
+> conda install -c anaconda jupyter
+
+3. Install spyder
+> conda install -c anaconda spyder=3.3.6
+
+4. Install yaml
+
+> pip install pyyaml==5.2
+
+5. Install imageio
+
+> conda install -c conda-forge imageio
+
+
+## Files
+
+1. Copy "ms1m_bin_in.tfrecord" to "data" folder
+- Training dataset in tensorflow format 
+2. Copy following folder files to "data\faces_test" folder
+> agedb_align_112
+cfp_align_112
+lfw_align_112
+- Testing dataset in aligned 112x112 image format
+3. Edit "arc_res50_bin.yaml" file at "config" folder
+- batch_size
+-- If OOM (out of memory) error, decrease the batch size to 64, 32 or 16
+-- Higher batch size reduces the training time
+- save_steps
+-- Control the checkpoints to be saved
+-- Increase checkpoints will reduces the saved checkpoints files, but may lost the results
+-- Only latest checkpoints or best checkpoints will be used for verification 
+
+## Training and Verification
+
+1. Training
+- Open "train_arc_res50_bin.py" in Spyder
+- Press "F5" to start training process
+- Note:
+-- When begin training process again, press "Ctrl + . " or select "Restart Kernel" to restart python kernel, then press "F5" to start new training process
+2. Verification
+- Copy best saved checkpoints files to "models" folder, and change their name to "arc_res50.ckpt.xxxx"
+- Open "test_arc_res50_bin.py" in Spyder
+- Press "F5" to start training process
+- Take same note as training process
---
+++ data/convert_train_tfrecord_from_mxdata.py
@@ -0,0 +1,124 @@
+from absl import app, flags, logging
+from absl.flags import FLAGS
+import os
+import io
+import shutil
+import imageio
+import numpy as np
+import cv2
+import tqdm
+import random
+import tensorflow as tf
+import mxnet as mx
+
+
+flags.DEFINE_string('training_dataset_path', './data', 'training dataset path')
+flags.DEFINE_string('dataset_path', 'faces_emore',
+                    'path to dataset')
+flags.DEFINE_string('output_path', 'ms1m_bin.tfrecord',
+                    'path to ouput tfrecord')
+
+
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy()
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def make_example(source_id, img_path):
+    # Create a dictionary with features that may be relevant.
+    feature = {'image/source_id': _int64_feature(source_id),
+               'image/img_path': _bytes_feature(img_path)}
+
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+def main(_):
+    dataset_path = FLAGS.dataset_path
+
+    if not os.path.isdir(dataset_path):
+        logging.info('Please define valid dataset path.')
+    else:
+        logging.info('Loading {}'.format(dataset_path))
+
+    idx_path = os.path.join(dataset_path, 'train.idx')
+    bin_path = os.path.join(dataset_path, 'train.rec')
+    img_path = os.path.join(dataset_path, 'img')
+
+    if os.path.exists(img_path):
+        shutil.rmtree(img_path)
+
+    with tf.io.TFRecordWriter(FLAGS.output_path) as writer:
+        mx_reader = mx.recordio.MXIndexedRecordIO(idx_path, bin_path, 'r')
+        first_rec = mx_reader.read_idx(0)
+        first_header, _ = mx.recordio.unpack(first_rec)
+        img_idx_list = list(range(1, int(first_header.label[0])))
+        samples = []
+        items_set = set()
+        
+        logging.info('Reading data list...')
+        for i in tqdm.tqdm(img_idx_list):
+            img_info = mx_reader.read_idx(i)
+            header, _ = mx.recordio.unpack(img_info)
+            img_label = int(header.label)
+            
+            #print(f'img_label: "{img_label}"')
+            img_id = int(header.id)
+            img_folder =  os.path.join(img_path, f'{img_label}')
+            if not os.path.exists(img_folder):
+                os.makedirs(img_folder)
+            img_filename = os.path.join(img_folder, f'{img_id}.jpg').replace("\\", "/")
+            #print(f'img_filename: "{img_filename}"')
+            
+            samples.append((img_id, img_label, img_filename))
+            
+            items_set.add(img_label)
+            
+            if i % 50000 == 0:
+                print(f'Stop at total {i} pictures, {len(items_set)} classes')
+                break
+        
+        random.shuffle(samples)
+        
+        logging.info('Writing tfrecord file...')
+        img_num = 0
+        for img_id, img_label, img_filename in tqdm.tqdm(samples):
+            img_info = mx_reader.read_idx(img_id)
+            header, img = mx.recordio.unpack(img_info)
+            
+            img = io.BytesIO(img)
+            img = imageio.imread(img).astype(np.uint8)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            #img = cv2.resize(img, (112,112))
+            #img_raw = img.tobytes()
+            
+            imageio.imwrite(img_filename, img)
+            img_train_path = os.path.join(FLAGS.training_dataset_path, 
+                                          img_filename).replace("\\", "/") 
+            
+            tf_example = make_example(source_id=img_label,
+                                      img_path=str.encode(img_train_path))
+            writer.write(tf_example.SerializeToString())
+            
+            img_num += 1
+            
+            if img_num % 10000 == 0:
+                print(f'Wrote total {img_num} pictures, {len(items_set)} classes')
+                break
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
