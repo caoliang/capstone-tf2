7f41f34090f6d2c4e1769769b8657afe5fa5c468
caoliang <sg_caoliang@yahoo.com.sg>
2020-02-29 18:22:58 +0800

    Update mtcnn codes that can load model weights;

--- mtcnn_tf2/detect_face_lib.py
+++ mtcnn_tf2/detect_face_lib.py
@@ -1,107 +1,468 @@
 import cv2
 import numpy as np
 import time
-from mtcnn_tf2.face_mtcnn import create_onet, create_rnet, create_pnet
+import logging
+from mtcnn_tf2.face_mtcnn import build_mtcnn_nets
 import mtcnn_tf2.tools_matrix as tools
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class StageStatus(object):
+    """
+    Keeps status between MTCNN stages
+    """
+
+    def __init__(self, pad_result: tuple = None, width=0, height=0):
+        self.width = width
+        self.height = height
+        self.dy = self.edy = self.dx = self.edx = self.y = self.ey = \
+            self.x = self.ex = self.tmpw = self.tmph = []
+
+        if pad_result is not None:
+            self.update(pad_result)
+
+    def update(self, pad_result: tuple):
+        s = self
+        s.dy, s.edy, s.dx, s.edx, s.y, s.ey, s.x, s.ex, s.tmpw, s.tmph = \
+            pad_result
 
 
 def create_mtcnn_nets():
-    pnet = create_pnet()
-    rnet = create_rnet()
-    onet = create_onet()
-    
+    pnet, rnet, onet = build_mtcnn_nets()
     return [pnet, rnet, onet]
 
 
-def detect_face(img, mtcnn_nets, threshold = [0.6,0.6,0.7]):
-    pnet, rnet, onet = mtcnn_nets
+def compute_scale_pyramid(m, min_layer, scale_factor):
+    scales = []
+    factor_count = 0
+
+    while min_layer >= 12:
+        scales += [m * np.power(scale_factor, factor_count)]
+        min_layer = min_layer * scale_factor
+        factor_count += 1
+
+    return scales
+
+def detect_faces(image = None, mtcnn_nets = None, min_face_size = 20, 
+                 scale_factor = 0.709, steps_threshold = [0.6, 0.7, 0.7]):
+    """
+    Detects bounding boxes from the specified image.
+    :param img: image to process
+    :return: list containing all the bounding boxes detected with their keypoints.
+    """    
+    total_boxes, points = detect_faces_features(image, mtcnn_nets, 
+                                                min_face_size, scale_factor,
+                                                steps_threshold)
+    binding_boxes = extract_binding_box(total_boxes, points)
     
-    cv_img = (img.copy() - 127.5) / 127.5
-    origin_h, origin_w, ch = cv_img.shape
-    scales = tools.calculateScales(img)
-    out = []
-    t0 = time.time()
-    # del scales[:4]
+    return binding_boxes
+    
 
+def detect_faces_features(image = None, mtcnn_nets = None, 
+                          min_face_size = 20, scale_factor = 0.709,
+                          steps_threshold = [0.6, 0.7, 0.7]):
+    if image is None or not hasattr(image, "shape"):
+        raise Exception("Face image is invalid")
+    
+    height, width, _ = image.shape
+    logger.info("image.shape {0}".format(image.shape))
+    stage_status = StageStatus(width=width, height=height)
+    
+    m = 12 / min_face_size
+    min_layer = np.amin([height, width]) * m
+    
+    scales = compute_scale_pyramid(m, min_layer, scale_factor)
+    
+    stages = [__stage1, __stage2, __stage3]
+    if mtcnn_nets is None:
+        # pnet, rnet, onet = mtcnn_nets
+        mtcnn_nets = create_mtcnn_nets()
+    
+    result = [scales, stage_status]
+    
+    # We pipe here each of the stages
+    for index in range(len(stages)):
+        result = stages[index](image, steps_threshold[index], 
+                       mtcnn_nets[index], result[0], result[1])
+    
+    [total_boxes, points] = result
+    
+    return total_boxes, points
+
+def extract_binding_box(all_boxes, all_points):
+    bounding_boxes = []
+    
+    for bounding_box, keypoints in zip(all_boxes, all_points.T):
+            bounding_boxes.append({
+                'box': [max(0, int(bounding_box[0])), max(0, int(bounding_box[1])),
+                        int(bounding_box[2] - bounding_box[0]), int(bounding_box[3] - bounding_box[1])],
+                'confidence': bounding_box[-1],
+                'keypoints': {
+                    'left_eye': (int(keypoints[0]), int(keypoints[5])),
+                    'right_eye': (int(keypoints[1]), int(keypoints[6])),
+                    'nose': (int(keypoints[2]), int(keypoints[7])),
+                    'mouth_left': (int(keypoints[3]), int(keypoints[8])),
+                    'mouth_right': (int(keypoints[4]), int(keypoints[9])),
+                }
+            })
+    
+    return bounding_boxes
+
+def __scale_image(image, scale: float):
+    """
+    Scales the image to a given scale.
+    :param image:
+    :param scale:
+    :return:
+    """
+    height, width, _ = image.shape
+
+    width_scaled = int(np.ceil(width * scale))
+    height_scaled = int(np.ceil(height * scale))
+
+    im_data = cv2.resize(image, (width_scaled, height_scaled), 
+                         interpolation=cv2.INTER_AREA)
+
+    # Normalize the image's pixels
+    im_data_normalized = (im_data - 127.5) * 0.0078125
+
+    return im_data_normalized
+
+
+def __generate_bounding_box(imap, reg, scale, box_threshold):
+
+    # use heatmap to generate bounding boxes
+    stride = 2
+    cellsize = 12
+    
+    imap = np.transpose(imap)
+    dx1 = np.transpose(reg[:, :, 0])
+    dy1 = np.transpose(reg[:, :, 1])
+    dx2 = np.transpose(reg[:, :, 2])
+    dy2 = np.transpose(reg[:, :, 3])
+    
+    y, x = np.where(imap >= box_threshold)
+    
+    if y.shape[0] == 1:
+        dx1 = np.flipud(dx1)
+        dy1 = np.flipud(dy1)
+        dx2 = np.flipud(dx2)
+        dy2 = np.flipud(dy2)
+    
+    score = imap[(y, x)]
+    reg = np.transpose(np.vstack([dx1[(y, x)], dy1[(y, x)], 
+                                  dx2[(y, x)], dy2[(y, x)]]))
+    
+    if reg.size == 0:
+        reg = np.empty(shape=(0, 3))
+    
+    bb = np.transpose(np.vstack([y, x]))
+    
+    q1 = np.fix((stride * bb + 1) / scale)
+    q2 = np.fix((stride * bb + cellsize) / scale)
+    boundingbox = np.hstack([q1, q2, np.expand_dims(score, 1), reg])
+    
+    return boundingbox, reg
+
+
+def __nms(boxes, threshold, method):
+    """
+    Non Maximum Suppression.
+
+    :param boxes: np array with bounding boxes.
+    :param threshold:
+    :param method: NMS method to apply. Available values ('Min', 'Union')
+    :return:
+    """
+    if boxes.size == 0:
+        return np.empty((0, 3))
+
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    s = boxes[:, 4]
+
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    sorted_s = np.argsort(s)
+
+    pick = np.zeros_like(s, dtype=np.int16)
+    counter = 0
+    while sorted_s.size > 0:
+        i = sorted_s[-1]
+        pick[counter] = i
+        counter += 1
+        idx = sorted_s[0:-1]
+
+        xx1 = np.maximum(x1[i], x1[idx])
+        yy1 = np.maximum(y1[i], y1[idx])
+        xx2 = np.minimum(x2[i], x2[idx])
+        yy2 = np.minimum(y2[i], y2[idx])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+
+        inter = w * h
+
+        if method is 'Min':
+            o = inter / np.minimum(area[i], area[idx])
+        else:
+            o = inter / (area[i] + area[idx] - inter)
+
+        sorted_s = sorted_s[np.where(o <= threshold)]
+
+    pick = pick[0:counter]
+
+    return pick
+
+
+def __pad(total_boxes, w, h):
+    # compute the padding coordinates (pad the bounding boxes to square)
+    tmpw = (total_boxes[:, 2] - total_boxes[:, 0] + 1).astype(np.int32)
+    tmph = (total_boxes[:, 3] - total_boxes[:, 1] + 1).astype(np.int32)
+    numbox = total_boxes.shape[0]
+    
+    dx = np.ones(numbox, dtype=np.int32)
+    dy = np.ones(numbox, dtype=np.int32)
+    edx = tmpw.copy().astype(np.int32)
+    edy = tmph.copy().astype(np.int32)
+    
+    x = total_boxes[:, 0].copy().astype(np.int32)
+    y = total_boxes[:, 1].copy().astype(np.int32)
+    ex = total_boxes[:, 2].copy().astype(np.int32)
+    ey = total_boxes[:, 3].copy().astype(np.int32)
+    
+    tmp = np.where(ex > w)
+    edx.flat[tmp] = np.expand_dims(-ex[tmp] + w + tmpw[tmp], 1)
+    ex[tmp] = w
+    
+    tmp = np.where(ey > h)
+    edy.flat[tmp] = np.expand_dims(-ey[tmp] + h + tmph[tmp], 1)
+    ey[tmp] = h
+    
+    tmp = np.where(x < 1)
+    dx.flat[tmp] = np.expand_dims(2 - x[tmp], 1)
+    x[tmp] = 1
+    
+    tmp = np.where(y < 1)
+    dy.flat[tmp] = np.expand_dims(2 - y[tmp], 1)
+    y[tmp] = 1
+    
+    return dy, edy, dx, edx, y, ey, x, ex, tmpw, tmph
+
+def __rerec(bbox):
+    # convert bbox to square
+    height = bbox[:, 3] - bbox[:, 1]
+    width = bbox[:, 2] - bbox[:, 0]
+    max_side_length = np.maximum(width, height)
+    bbox[:, 0] = bbox[:, 0] + width * 0.5 - max_side_length * 0.5
+    bbox[:, 1] = bbox[:, 1] + height * 0.5 - max_side_length * 0.5
+    bbox[:, 2:4] = bbox[:, 0:2] + np.transpose(np.tile(max_side_length, (2, 1)))
+    return bbox
+
+def __bbreg(boundingbox, reg):
+    # calibrate bounding boxes
+    if reg.shape[1] == 1:
+        reg = np.reshape(reg, (reg.shape[2], reg.shape[3]))
+    
+    w = boundingbox[:, 2] - boundingbox[:, 0] + 1
+    h = boundingbox[:, 3] - boundingbox[:, 1] + 1
+    b1 = boundingbox[:, 0] + reg[:, 0] * w
+    b2 = boundingbox[:, 1] + reg[:, 1] * h
+    b3 = boundingbox[:, 2] + reg[:, 2] * w
+    b4 = boundingbox[:, 3] + reg[:, 3] * h
+    boundingbox[:, 0:4] = np.transpose(np.vstack([b1, b2, b3, b4]))
+    return boundingbox
+
+
+def __stage1(img, step_threshold, mtcnn_net, scales, stage_status):
+    """
+    First stage of the MTCNN.
+    :param image:
+    :param mtcnn_net:
+    :param scales:
+    :param stage_status:
+    :return:
+    """
+    total_boxes = np.empty((0, 9))
+    status = stage_status
+    
     for scale in scales:
-        hs = int(origin_h * scale)
-        ws = int(origin_w * scale)
-        scale_img = cv2.resize(cv_img, (ws, hs))
-        input = scale_img.reshape(1, *scale_img.shape)
-        ouput = pnet.predict(input)  # .transpose(0,2,1,3) should add, but seems after process is wrong then.
-        out.append(ouput)
-    image_num = len(scales)
-    rectangles = []
-    for i in range(image_num):
-        cls_prob = out[i][0][0][:, :,
-                   1]  # i = #scale, first 0 select cls score, second 0 = batchnum, alway=0. 1 one hot repr
-        roi = out[i][1][0]
-        out_h, out_w = cls_prob.shape
-        out_side = max(out_h, out_w)
-        # print('calculating img scale #:', i)
-        cls_prob = np.swapaxes(cls_prob, 0, 1)
-        roi = np.swapaxes(roi, 0, 2)
-        rectangle = tools.detect_face_12net(cls_prob, roi, out_side, 1 / scales[i], origin_w, origin_h, threshold[0])
-        rectangles.extend(rectangle)
-    rectangles = tools.NMS(rectangles, 0.7, 'iou')
-
-    t1 = time.time()
-    print ('time for 12 net is: ', t1-t0)
-
-    if len(rectangles) == 0:
-        return rectangles
-
-    crop_number = 0
-    out = []
-    predict_24_batch = []
-    for rectangle in rectangles:
-        crop_img = cv_img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
-        scale_img = cv2.resize(crop_img, (24, 24))
-        predict_24_batch.append(scale_img)
-        crop_number += 1
-
-    predict_24_batch = np.array(predict_24_batch)
-
-    out = rnet.predict(predict_24_batch)
-
-    cls_prob = out[0]  # first 0 is to select cls, second batch number, always =0
-    cls_prob = np.array(cls_prob)  # convert to numpy
-    roi_prob = out[1]  # first 0 is to select roi, second batch number, always =0
-    roi_prob = np.array(roi_prob)
-    rectangles = tools.filter_face_24net(cls_prob, roi_prob, rectangles, origin_w, origin_h, threshold[1])
-    t2 = time.time()
-    print ('time for 24 net is: ', t2-t1)
+        scaled_image = __scale_image(img, scale)
+    
+        img_x = np.expand_dims(scaled_image, 0)
+        img_y = np.transpose(img_x, (0, 2, 1, 3))
+    
+        out = mtcnn_net.predict(img_y)
+    
+        out0 = np.transpose(out[0], (0, 2, 1, 3))
+        out1 = np.transpose(out[1], (0, 2, 1, 3))
+    
+        boxes, _ = __generate_bounding_box(out1[0, :, :, 1].copy(),
+                                           out0[0, :, :, :].copy(), 
+                                           scale, step_threshold)
+    
+        # inter-scale nms
+        pick = __nms(boxes.copy(), 0.5, 'Union')
+        if boxes.size > 0 and pick.size > 0:
+            boxes = boxes[pick, :]
+            total_boxes = np.append(total_boxes, boxes, axis=0)
+    
+    numboxes = total_boxes.shape[0]
+    
+    if numboxes > 0:
+        pick = __nms(total_boxes.copy(), 0.7, 'Union')
+        total_boxes = total_boxes[pick, :]
+    
+        regw = total_boxes[:, 2] - total_boxes[:, 0]
+        regh = total_boxes[:, 3] - total_boxes[:, 1]
+    
+        qq1 = total_boxes[:, 0] + total_boxes[:, 5] * regw
+        qq2 = total_boxes[:, 1] + total_boxes[:, 6] * regh
+        qq3 = total_boxes[:, 2] + total_boxes[:, 7] * regw
+        qq4 = total_boxes[:, 3] + total_boxes[:, 8] * regh
+    
+        total_boxes = np.transpose(np.vstack([qq1, qq2, qq3, qq4, 
+                                              total_boxes[:, 4]]))
+        total_boxes = __rerec(total_boxes.copy())
+    
+        total_boxes[:, 0:4] = np.fix(total_boxes[:, 0:4]).astype(np.int32)
+        status = StageStatus(__pad(total_boxes.copy(), stage_status.width, 
+                                   stage_status.height),
+                             width=stage_status.width, 
+                             height=stage_status.height)
+    
+    return total_boxes, status
 
 
-    if len(rectangles) == 0:
-        return rectangles
+def __stage2(img, step_threshold, mtcnn_net, total_boxes, stage_status):
+    """
+    Second stage of the MTCNN.
+    :param img:
+    :param step_threshold:
+    :param total_boxes:
+    :param stage_status:
+    :return:
+    """
+
+    num_boxes = total_boxes.shape[0]
+    if num_boxes == 0:
+        return total_boxes, stage_status
+
+    # second stage
+    tempimg = np.zeros(shape=(24, 24, 3, num_boxes))
+
+    for k in range(0, num_boxes):
+        tmp = np.zeros((int(stage_status.tmph[k]), 
+                        int(stage_status.tmpw[k]), 3))
+
+        tmp[stage_status.dy[k] - 1:stage_status.edy[k], 
+            stage_status.dx[k] - 1:stage_status.edx[k], :] = \
+            img[stage_status.y[k] - 1:stage_status.ey[k],
+                stage_status.x[k] - 1:stage_status.ex[k], :]
+
+        if (tmp.shape[0] > 0 and tmp.shape[1] > 0 or tmp.shape[0] == 0 
+            and tmp.shape[1] == 0):
+            
+            tempimg[:, :, :, k] = cv2.resize(tmp, (24, 24), 
+                                  interpolation=cv2.INTER_AREA)
+
+        else:
+            return np.empty(shape=(0,)), stage_status
+
+    tempimg = (tempimg - 127.5) * 0.0078125
+    tempimg1 = np.transpose(tempimg, (3, 1, 0, 2))
+
+    out = mtcnn_net.predict(tempimg1)
+
+    out0 = np.transpose(out[0])
+    out1 = np.transpose(out[1])
+
+    score = out1[1, :]
+
+    ipass = np.where(score > step_threshold)
+
+    total_boxes = np.hstack([total_boxes[ipass[0], 0:4].copy(), 
+                             np.expand_dims(score[ipass].copy(), 1)])
+
+    mv = out0[:, ipass[0]]
+
+    if total_boxes.shape[0] > 0:
+        pick = __nms(total_boxes, 0.7, 'Union')
+        total_boxes = total_boxes[pick, :]
+        total_boxes = __bbreg(total_boxes.copy(), np.transpose(mv[:, pick]))
+        total_boxes = __rerec(total_boxes.copy())
+
+    return total_boxes, stage_status
 
 
-    crop_number = 0
-    predict_batch = []
-    for rectangle in rectangles:
-        # print('calculating net 48 crop_number:', crop_number)
-        crop_img = cv_img[int(rectangle[1]):int(rectangle[3]), int(rectangle[0]):int(rectangle[2])]
-        scale_img = cv2.resize(crop_img, (48, 48))
-        predict_batch.append(scale_img)
-        crop_number += 1
+def __stage3(img, step_threshold, mtcnn_net, total_boxes, stage_status):
+    """
+    Third stage of the MTCNN.
 
-    predict_batch = np.array(predict_batch)
+    :param img:
+    :param total_boxes:
+    :param stage_status:
+    :return:
+    """
+    num_boxes = total_boxes.shape[0]
+    if num_boxes == 0:
+        return total_boxes, np.empty(shape=(0,))
 
-    output = onet.predict(predict_batch)
-    cls_prob = output[0]
-    roi_prob = output[1]
-    pts_prob = output[2]  # index
-    # rectangles = tools.filter_face_48net_newdef(cls_prob, roi_prob, pts_prob, rectangles, origin_w, origin_h,
-    #                                             threshold[2])
-    rectangles = tools.filter_face_48net(cls_prob, roi_prob, pts_prob, rectangles, origin_w, origin_h, threshold[2])
-    t3 = time.time()
-    print ('time for 48 net is: ', t3-t2)
+    total_boxes = np.fix(total_boxes).astype(np.int32)
 
-    return rectangles
+    status = StageStatus(__pad(total_boxes.copy(), stage_status.width, 
+                                    stage_status.height),
+                         width=stage_status.width, height=stage_status.height)
 
+    tempimg = np.zeros((48, 48, 3, num_boxes))
 
+    for k in range(0, num_boxes):
 
+        tmp = np.zeros((int(status.tmph[k]), int(status.tmpw[k]), 3))
 
+        tmp[status.dy[k] - 1:status.edy[k], status.dx[k] - 1:status.edx[k], :] = \
+            img[status.y[k] - 1:status.ey[k], status.x[k] - 1:status.ex[k], :]
 
+        if tmp.shape[0] > 0 and tmp.shape[1] > 0 or tmp.shape[0] == 0 and tmp.shape[1] == 0:
+            tempimg[:, :, :, k] = cv2.resize(tmp, (48, 48), interpolation=cv2.INTER_AREA)
+        else:
+            return np.empty(shape=(0,)), np.empty(shape=(0,))
 
+    tempimg = (tempimg - 127.5) * 0.0078125
+    tempimg1 = np.transpose(tempimg, (3, 1, 0, 2))
+
+    out = mtcnn_net.predict(tempimg1)
+    out0 = np.transpose(out[0])
+    out1 = np.transpose(out[1])
+    out2 = np.transpose(out[2])
+
+    score = out2[1, :]
+
+    points = out1
+
+    ipass = np.where(score > step_threshold)
+
+    points = points[:, ipass[0]]
+
+    total_boxes = np.hstack([total_boxes[ipass[0], 0:4].copy(), 
+                             np.expand_dims(score[ipass].copy(), 1)])
+
+    mv = out0[:, ipass[0]]
+
+    w = total_boxes[:, 2] - total_boxes[:, 0] + 1
+    h = total_boxes[:, 3] - total_boxes[:, 1] + 1
+
+    points[0:5, :] = np.tile(w, (5, 1)) * points[0:5, :] + \
+                     np.tile(total_boxes[:, 0], (5, 1)) - 1
+    points[5:10, :] = np.tile(h, (5, 1)) * points[5:10, :] + \
+                      np.tile(total_boxes[:, 1], (5, 1)) - 1
+
+    if total_boxes.shape[0] > 0:
+        total_boxes = __bbreg(total_boxes.copy(), np.transpose(mv))
+        pick = __nms(total_boxes.copy(), 0.7, 'Min')
+        total_boxes = total_boxes[pick, :]
+        points = points[:, pick]
+
+    return total_boxes, points
--- mtcnn_tf2/face_mtcnn.py
+++ mtcnn_tf2/face_mtcnn.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Input, MaxPool2D
 from tensorflow.keras.layers import Reshape, Activation, Flatten
-from tensorflow.keras.layers import Dense, Permute, PReLU
+from tensorflow.keras.layers import Dense, Permute, Softmax, PReLU
 from tensorflow.keras.models import Model, Sequential
 import numpy as np
 import os
@@ -13,76 +13,107 @@ def get_weight_path(weight_filename):
     print(weight_path)
     return weight_path
 
-def create_onet(weight_path = None):
-    if weight_path is None:
-        weight_path = get_weight_path('onet.h5')
-        
-    input = Input(shape = [48,48,3])
-    x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv1')(input)
-    x = PReLU(shared_axes=[1,2],name='prelu1')(x)
-    x = MaxPool2D(pool_size=3, strides=2, padding='same')(x)
-    x = Conv2D(64, (3, 3), strides=1, padding='valid', name='conv2')(x)
-    x = PReLU(shared_axes=[1,2],name='prelu2')(x)
-    x = MaxPool2D(pool_size=3, strides=2)(x)
-    x = Conv2D(64, (3, 3), strides=1, padding='valid', name='conv3')(x)
-    x = PReLU(shared_axes=[1,2],name='prelu3')(x)
-    x = MaxPool2D(pool_size=2)(x)
-    x = Conv2D(128, (2, 2), strides=1, padding='valid', name='conv4')(x)
-    x = PReLU(shared_axes=[1,2],name='prelu4')(x)
-    x = Permute((3,2,1))(x)
-    x = Flatten()(x)
-    x = Dense(256, name='conv5') (x)
-    x = PReLU(name='prelu5')(x)
+def create_onet(input_shape=None):
+    if input_shape is None:
+        input_shape = (48, 48, 3)
 
-    classifier = Dense(2, activation='softmax',name='conv6-1')(x)
-    bbox_regress = Dense(4,name='conv6-2')(x)
-    landmark_regress = Dense(10,name='conv6-3')(x)
-    model = Model([input], [classifier, bbox_regress, landmark_regress])
-    model.load_weights(weight_path, by_name=True)
+    o_inp = Input(input_shape)
+    o_layer = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding="valid")(o_inp)
+    o_layer = PReLU(shared_axes=[1, 2])(o_layer)
+    o_layer = MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="same")(o_layer)
 
-    return model
+    o_layer = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding="valid")(o_layer)
+    o_layer = PReLU(shared_axes=[1, 2])(o_layer)
+    o_layer = MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="valid")(o_layer)
 
+    o_layer = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding="valid")(o_layer)
+    o_layer = PReLU(shared_axes=[1, 2])(o_layer)
+    o_layer = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="same")(o_layer)
 
-def create_rnet (weight_path = None):
-    if weight_path is None:
-        weight_path = get_weight_path('rnet.h5')
-        
-    input = Input(shape=[24, 24, 3])  # change this shape to [None,None,3] to enable arbitraty shape input
-    x = Conv2D(28, (3, 3), strides=1, padding='valid', name='conv1')(input)
-    x = PReLU(shared_axes=[1, 2], name='prelu1')(x)
-    x = MaxPool2D(pool_size=3,strides=2, padding='same')(x)
+    o_layer = Conv2D(128, kernel_size=(2, 2), strides=(1, 1), padding="valid")(o_layer)
+    o_layer = PReLU(shared_axes=[1, 2])(o_layer)
 
-    x = Conv2D(48, (3, 3), strides=1, padding='valid', name='conv2')(x)
-    x = PReLU(shared_axes=[1, 2], name='prelu2')(x)
-    x = MaxPool2D(pool_size=3, strides=2)(x)
+    o_layer = Flatten()(o_layer)
+    o_layer = Dense(256)(o_layer)
+    o_layer = PReLU()(o_layer)
 
-    x = Conv2D(64, (2, 2), strides=1, padding='valid', name='conv3')(x)
-    x = PReLU(shared_axes=[1, 2], name='prelu3')(x)
-    x = Permute((3, 2, 1))(x)
-    x = Flatten()(x)
-    x = Dense(128, name='conv4')(x)
-    x = PReLU( name='prelu4')(x)
-    classifier = Dense(2, activation='softmax', name='conv5-1')(x)
-    bbox_regress = Dense(4, name='conv5-2')(x)
-    model = Model([input], [classifier, bbox_regress])
-    model.load_weights(weight_path, by_name=True)
-    return model
+    o_layer_out1 = Dense(2)(o_layer)
+    o_layer_out1 = Softmax(axis=1)(o_layer_out1)
+    o_layer_out2 = Dense(4)(o_layer)
+    o_layer_out3 = Dense(10)(o_layer)
+
+    o_net = Model(o_inp, [o_layer_out2, o_layer_out3, o_layer_out1])
+    return o_net
 
 
-def create_pnet( weight_path = None):
-    if weight_path is None:
-        weight_path = get_weight_path('pnet.h5')
+def create_rnet (input_shape=None):
+    if input_shape is None:
+        input_shape = (24, 24, 3)
+
+    r_inp = Input(input_shape)
+
+    r_layer = Conv2D(28, kernel_size=(3, 3), strides=(1, 1), padding="valid")(r_inp)
+    r_layer = PReLU(shared_axes=[1, 2])(r_layer)
+    r_layer = MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="same")(r_layer)
+
+    r_layer = Conv2D(48, kernel_size=(3, 3), strides=(1, 1), padding="valid")(r_layer)
+    r_layer = PReLU(shared_axes=[1, 2])(r_layer)
+    r_layer = MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="valid")(r_layer)
+
+    r_layer = Conv2D(64, kernel_size=(2, 2), strides=(1, 1), padding="valid")(r_layer)
+    r_layer = PReLU(shared_axes=[1, 2])(r_layer)
+    r_layer = Flatten()(r_layer)
+    r_layer = Dense(128)(r_layer)
+    r_layer = PReLU()(r_layer)
+
+    r_layer_out1 = Dense(2)(r_layer)
+    r_layer_out1 = Softmax(axis=1)(r_layer_out1)
+
+    r_layer_out2 = Dense(4)(r_layer)
+
+    r_net = Model(r_inp, [r_layer_out2, r_layer_out1])
+
+    return r_net
+
+
+def create_pnet(input_shape=None):
+    if input_shape is None:
+        input_shape = (None, None, 3)
+
+    p_inp = Input(input_shape)
+
+    p_layer = Conv2D(10, kernel_size=(3, 3), strides=(1, 1), padding="valid")(p_inp)
+    p_layer = PReLU(shared_axes=[1, 2])(p_layer)
+    p_layer = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="same")(p_layer)
+
+    p_layer = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), padding="valid")(p_layer)
+    p_layer = PReLU(shared_axes=[1, 2])(p_layer)
+
+    p_layer = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding="valid")(p_layer)
+    p_layer = PReLU(shared_axes=[1, 2])(p_layer)
+
+    p_layer_out1 = Conv2D(2, kernel_size=(1, 1), strides=(1, 1))(p_layer)
+    p_layer_out1 = Softmax(axis=3)(p_layer_out1)
+
+    p_layer_out2 = Conv2D(4, kernel_size=(1, 1), strides=(1, 1))(p_layer)
+
+    p_net = Model(p_inp, [p_layer_out2, p_layer_out1])
+   
+    return p_net
+
+
+def build_mtcnn_nets(weights_files = None):
+    if weights_files is None:
+        weights_files = ['pnet.h5', 'rnet.h5', 'onet.h5'] 
+                         
+    p_net = create_pnet()
+    r_net = create_rnet()
+    o_net = create_onet()
+
+    nets = [p_net, r_net, o_net]
     
-    input = Input(shape=[None, None, 3])
-    x = Conv2D(10, (3, 3), strides=1, padding='valid', name='conv1')(input)
-    x = PReLU(shared_axes=[1,2],name='PReLU1')(x)
-    x = MaxPool2D(pool_size=2)(x)
-    x = Conv2D(16, (3, 3), strides=1, padding='valid', name='conv2')(x)
-    x = PReLU(shared_axes=[1,2],name='PReLU2')(x)
-    x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv3')(x)
-    x = PReLU(shared_axes=[1,2],name='PReLU3')(x)
-    classifier = Conv2D(2, (1, 1), activation='softmax', name='conv4-1')(x)
-    bbox_regress = Conv2D(4, (1, 1), name='conv4-2')(x)
-    model = Model([input], [classifier, bbox_regress])
-    model.load_weights(weight_path, by_name=True)
-    return model
+    for index in range(len(nets)):
+        weights_path = get_weight_path(weights_files[index])
+        nets[index].load_weights(weights_path)
+
+    return p_net, r_net, o_net
